c96fcc5eaf2297a494c2da8a89606206ca336570
André Palmborg <andpa149@student.liu.se>
2021-04-06 13:37:33 +0200

    Indentation fix

--- u100-the-3n+1-problem/p100.c
+++ u100-the-3n+1-problem/p100.c
@@ -2,47 +2,50 @@
 
 #include <stdio.h>
 
-int cycle(a) int a;
+int cycle(int a)
 {
-	int i = 1;
+    int i = 1;
 
-  while (a != 1){
-		if (a % 2 == 0){
-	    a = a/2;
-		}
-		else{
-			a = 3*a+1;
-		}
-		i++;
-  }
-  return i;
+    while( a != 1 )
+    {
+        if( a % 2 == 0 ) {
+            a = a/2;
+        } else {
+            a = 3*a+1;
+        }
+        i++;
+    }
+    return i;
 }
 
 
 int main()
 {
-    int a,b,max,temp;
-    int aOrig,bOrig;
+    int a, b, max, temp;
+    int aOrig, bOrig;
     int i;
 
-    while (scanf("%d %d\n",&a,&b)==2){
-	
-			aOrig = a;
-			bOrig = b;
+    while( scanf("%d %d\n",&a,&b)==2 ) {
 
-			if (a > b){
-					temp = a;
-					a = b;
-					b = temp;
-			}
-	
-			max = cycle(a);
-			for(i=a+1; i<=b; i++) {
-					temp = cycle(i);
-					if (temp > max)
-				max = temp;
-			}
-			printf("%d %d %d\n", aOrig, bOrig,max);
+        aOrig = a;
+        bOrig = b;
+
+        if (a > b)
+        {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        max = cycle(a);
+        for( i=a+1; i<=b; i++ )
+        {
+            temp = cycle(i);
+            if(temp > max) {
+                max = temp;
+            }
+        }
+        printf("%d %d %d\n", aOrig, bOrig, max);
     }
-    return(0);
+    return 0;
 }
